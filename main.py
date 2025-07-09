83e9061e9f342e17c94b59d1d2111a77098d44e0
GabrielTheDEV <gabriel4321hh@gmail.com>
2025-07-09 20:25:58 -0300

    import_spreadsheet implementado

---
+++ main.py
@@ -0,0 +1,5 @@
+
+
+print(text)
+# with open('data/p.xlsx', 'rb') as file:
+#     print(file)
---
+++ src/utils/read_spreadsheet.py
@@ -0,0 +1,7 @@
+from import_spreadsheet import import_spreadsheet
+
+worksheet = import_spreadsheet('data/p.xlsx')
+
+def read_spreadsheet():
+    print('Hello world')
+    
