ff7ad45f4ced9de4e70ccb81a2aa89f5fd7e5d74
GabrielTheDEV <gabriel4321hh@gmail.com>
2025-07-20 21:25:02 -0300

    email_list

--- main.py
+++ main.py
@@ -3,26 +3,27 @@ from src.messaging.delivery_message import delivery_message
 from src.messaging.delay_message import delay_message
 from src.messaging.financial_message import financial_message
 # from src.utils.read_emails import read_emails
+from src.utils.email_list import email_list
 
 delivery_report = delivery_message()
 financial_report = financial_message()
 delay_report = delay_message()
+emails = email_list()
 
 send_email(
-    to=['gabrielkk290h@gmail.com'],
+    to=emails,
     subject=delivery_report['subject'],
     body=delivery_report['body']
 )
 send_email(
-    to=['gabrielkk290h@gmail.com'],
+    to=emails,
     subject=delay_report['subject'],
     body=delay_report['body']
         
 )
 send_email(
-    to=['gabrielkk290h@gmail.com'],
+    to=emails,
     subject=financial_report['subject'],
     body=financial_report['body']
         
 )
-
--- src/utils/email_list.py
+++ src/utils/email_list.py
@@ -1,2 +1,8 @@
 import os
 
+
+def email_list():
+    path = 'data/emails.txt'
+    with open(path , 'r') as file:
+        email = file.readlines()
+        return email
